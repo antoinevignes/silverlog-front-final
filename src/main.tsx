a04541674991e41fa89e2127f3260cfa2c673d0d
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-01-24 11:01:26 +0100

    setup projet

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "./styles/main.scss";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 
 import type { QueryClient } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -10,6 +11,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
+      <Toaster richColors />
     </>
   ),
 });
