ebed2c3ef2368cc361cf6358974d8d1a5073ccc1
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-01-25 14:23:51 +0100

    injection queryclient directement dans main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,21 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import * as TanStackQueryProvider from "./integrations/tanstack-query/root-provider.tsx";
-
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 import reportWebVitals from "./reportWebVitals.ts";
 import { AuthProvider, useAuth } from "./auth.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a new router instance
 
-const TanStackQueryProviderContext = TanStackQueryProvider.getContext();
+const queryClient = new QueryClient();
 const router = createRouter({
   routeTree,
   context: {
     auth: undefined!,
-    ...TanStackQueryProviderContext,
+    queryClient: undefined!,
   },
   defaultPreload: "intent",
   scrollRestoration: true,
@@ -44,11 +43,11 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <TanStackQueryProvider.Provider {...TanStackQueryProviderContext}>
+      <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <InnerApp />
         </AuthProvider>
-      </TanStackQueryProvider.Provider>
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
