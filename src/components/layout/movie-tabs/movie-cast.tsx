889103b3ee4ea17cf2ffb47d799107fa35acd32d
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-02-01 18:46:41 +0100

    ajout des liens vers les acteurs et le crew

--- src/components/layout/movie-tabs/movie-cast.tsx
+++ src/components/layout/movie-tabs/movie-cast.tsx
@@ -1,7 +1,7 @@
 import { movieCreditsQuery } from "@/queries/movie.queries";
 import type { CastType } from "@/utils/types/cast";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { getRouteApi } from "@tanstack/react-router";
+import { getRouteApi, Link } from "@tanstack/react-router";
 import HorizontalScroller from "../horizontal-scroller/horizontal-scroller";
 
 export default function MovieCast() {
@@ -17,16 +17,18 @@ export default function MovieCast() {
     <HorizontalScroller className="cast-scroller">
       {cast.map((actor) => (
         <li key={actor.id} className="actor-crew-card">
-          <div className="actor-crew-image">
-            <img
-              src={`https://image.tmdb.org/t/p/w200/${actor.profile_path}`}
-              alt={`Image de ${actor.name}`}
-            />
-          </div>
+          <Link to="/person/$personId" params={{ personId: String(actor.id) }}>
+            <div className="actor-crew-image">
+              <img
+                src={`https://image.tmdb.org/t/p/w200/${actor.profile_path}`}
+                alt={`Image de ${actor.name}`}
+              />
+            </div>
 
-          <p className="actor-crew-details">
-            {actor.name} ({actor.character})
-          </p>
+            <p className="actor-crew-details link">
+              {actor.name} ({actor.character})
+            </p>
+          </Link>
         </li>
       ))}
     </HorizontalScroller>
--- src/components/layout/movie-tabs/movie-crew.tsx
+++ src/components/layout/movie-tabs/movie-crew.tsx
@@ -1,7 +1,7 @@
 import { movieCreditsQuery } from "@/queries/movie.queries";
 import type { CrewType } from "@/utils/types/crew";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { getRouteApi } from "@tanstack/react-router";
+import { getRouteApi, Link } from "@tanstack/react-router";
 import HorizontalScroller from "../horizontal-scroller/horizontal-scroller";
 
 export default function MovieCrew() {
@@ -18,16 +18,21 @@ export default function MovieCrew() {
       <HorizontalScroller className="cast-scroller">
         {crew.map((member) => (
           <li key={member.id} className="actor-crew-card">
-            <div className="actor-crew-image">
-              <img
-                src={`https://image.tmdb.org/t/p/w200/${member.profile_path}`}
-                alt={`Image de ${member.name}`}
-              />
-            </div>
+            <Link
+              to="/person/$personId"
+              params={{ personId: String(member.id) }}
+            >
+              <div className="actor-crew-image">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200/${member.profile_path}`}
+                  alt={`Image de ${member.name}`}
+                />
+              </div>
 
-            <p className="actor-crew-details">
-              {member.name} ({member.job})
-            </p>
+              <p className="actor-crew-details link">
+                {member.name} ({member.job})
+              </p>
+            </Link>
           </li>
         ))}
       </HorizontalScroller>
