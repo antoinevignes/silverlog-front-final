c9194e23e1c514cb87fed02d9d3e7952ccd53dca
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-02-23 16:49:40 +0100

    ajout tabs

--- src/routes/_authenticated/user/$userId/activity/index.tsx
+++ src/routes/_authenticated/user/$userId/activity/index.tsx
@@ -1,10 +1,17 @@
 import { useAuth } from "@/auth";
 import TopList from "@/components/layout/activity/top-list/top-list";
 import Watchlist from "@/components/layout/activity/watchlist/watchlist";
+import Tabs from "@/components/ui/tabs/tabs";
 import { listDataQuery } from "@/queries/list.queries";
 import type { MovieType } from "@/utils/types/movie";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+
+const tabs = [
+  { id: "watchlist", label: "Watchlist" },
+  { id: "top-list", label: "Mon Top" },
+];
 
 export const Route = createFileRoute("/_authenticated/user/$userId/activity/")({
   loader: ({ context }) => {
@@ -20,62 +27,89 @@ export const Route = createFileRoute("/_authenticated/user/$userId/activity/")({
 
 function RouteComponent() {
   const { user } = useAuth();
-  const { data: topListData, isLoading: isLoadingTopList } = useQuery(
-    listDataQuery(user!.top_list_id!),
-  );
+
+  const [selected, setSelected] = useState<string>("watchlist");
+
   const { data: watchlistData, isLoading: isLoadingWatchlist } = useQuery(
     listDataQuery(user!.watchlist_id!),
   );
 
-  const topListMoviesDetailsResults = useQueries({
-    queries: (topListData ?? []).map((item: any) => ({
-      queryKey: ["movie", item.movie_id, "details", item.added_at],
-      queryFn: async () => {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        const res = await fetch(
-          `${import.meta.env.VITE_API_URL}/tmdb/movie/${item.movie_id}?language=fr-FR`,
-        );
-        const details = await res.json();
-
-        return details;
-      },
-      staleTime: 1000 * 60 * 60 * 24,
-    })),
-  });
+  const { data: topListData, isLoading: isLoadingTopList } = useQuery(
+    listDataQuery(user!.top_list_id!),
+  );
 
   const watchlistMoviesDetailsResults = useQueries({
-    queries: (watchlistData ?? []).map((item: any) => ({
-      queryKey: ["movie", item.movie_id, "details", item.added_at],
-      queryFn: async () => {
-        const res = await fetch(
-          `${import.meta.env.VITE_API_URL}/tmdb/movie/${item.movie_id}?language=fr-FR`,
-        );
-        const details = await res.json();
+    queries:
+      selected === "watchlist"
+        ? (watchlistData ?? []).map((item: any) => ({
+            queryKey: ["movie", item.movie_id, "details", item.added_at],
+            queryFn: async () => {
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+              const res = await fetch(
+                `${import.meta.env.VITE_API_URL}/tmdb/movie/${item.movie_id}?language=fr-FR`,
+              );
+              const details = await res.json();
 
-        return details;
-      },
-      staleTime: 1000 * 60 * 60 * 24,
-    })),
+              return details;
+            },
+            staleTime: 1000 * 60 * 60 * 24,
+          }))
+        : [],
   });
 
-  const topListMovies = topListMoviesDetailsResults
-    .map((r) => r.data)
-    .filter(Boolean) as MovieType[];
+  const topListMoviesDetailsResults = useQueries({
+    queries:
+      selected === "top-list"
+        ? (topListData ?? []).map((item: any) => ({
+            queryKey: ["movie", item.movie_id, "details", item.added_at],
+            queryFn: async () => {
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+              const res = await fetch(
+                `${import.meta.env.VITE_API_URL}/tmdb/movie/${item.movie_id}?language=fr-FR`,
+              );
+              const details = await res.json();
+
+              return details;
+            },
+            staleTime: 1000 * 60 * 60 * 24,
+          }))
+        : [],
+  });
 
   const watchlistMovies = watchlistMoviesDetailsResults
     .map((r) => r.data)
     .filter(Boolean) as MovieType[];
 
+  const topListMovies = topListMoviesDetailsResults
+    .map((r) => r.data)
+    .filter(Boolean) as MovieType[];
+
   const isFetchingMovies =
-    isLoadingTopList ||
-    isLoadingWatchlist ||
-    topListMoviesDetailsResults.some((r) => r.isLoading) ||
-    watchlistMoviesDetailsResults.some((r) => r.isLoading);
+    selected === "watchlist"
+      ? isLoadingWatchlist ||
+        watchlistMoviesDetailsResults.some((r) => r.isLoading)
+      : isLoadingTopList ||
+        topListMoviesDetailsResults.some((r) => r.isLoading);
 
   return (
     <>
-      <TopList isFetchingMovies={isFetchingMovies} movies={topListMovies} />
-      <Watchlist isFetchingMovies={isFetchingMovies} movies={watchlistMovies} />
+      <Tabs
+        selected={selected}
+        setSelected={setSelected}
+        tabs={tabs}
+        variant="header"
+      />
+
+      {selected === "watchlist" && (
+        <Watchlist
+          isFetchingMovies={isFetchingMovies}
+          movies={watchlistMovies}
+        />
+      )}
+
+      {selected === "top-list" && (
+        <TopList isFetchingMovies={isFetchingMovies} movies={topListMovies} />
+      )}
     </>
   );
 }
