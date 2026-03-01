1773648632672588d0cd2f729d44f2ce4bb892bc
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-03-01 15:05:21 +0100

    ajout composant title

--- src/components/layout/title/title.tsx
+++ src/components/layout/title/title.tsx
@@ -6,17 +6,19 @@ export default function Title({
   variant = "h2",
   size = "md",
   className = "",
+  id,
 }: {
   title: string;
   subtitle?: string;
   variant?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   size?: "sm" | "md" | "lg" | "xl";
   className?: string;
+  id?: string;
 }) {
   const Tag = variant;
 
   return (
-    <div className={`title-container ${className}`}>
+    <div className={`title-container ${className}`} id={id}>
       <Tag className={`font-sentient truncate-2-lines title ${size}`}>
         {title}
       </Tag>
