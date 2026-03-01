8378df55a069191d18cc4ed2543f585080b029ce
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-03-01 17:25:22 +0100

    correction mobile-nav

--- src/components/layout/mobile-nav/mobile-nav.tsx
+++ src/components/layout/mobile-nav/mobile-nav.tsx
@@ -10,48 +10,51 @@ export default function MobileNav() {
     <nav className="mobile-bottom-nav">
       <ul className="nav-items">
         <li>
-          <Link
-            to="/"
-            className="nav-link"
-            activeProps={{ className: "active" }}
-            activeOptions={{ exact: true }}
-          >
+          <Link to="/" className="nav-link">
             <Home size={24} />
             <span>Accueil</span>
           </Link>
         </li>
         <li>
-          <Link
-            to="/lists"
-            className="nav-link"
-            activeProps={{ className: "active" }}
-          >
+          <Link to="/lists" className="nav-link">
             <ListVideo size={24} />
             <span>Listes</span>
           </Link>
         </li>
         <li>
-          <Link
-            to="/user/activity"
-            className="nav-link"
-            activeProps={{ className: "active" }}
-          >
+          <Link to="/user/activity" className="nav-link">
             <Bookmark size={24} />
             <span>Watchlist</span>
           </Link>
         </li>
-        <li>
-          <Link
-            to="/user/$userId"
-            params={{ userId: user?.id?.toString() || "" }}
-            className="nav-link"
-            activeProps={{ className: "active" }}
-            aria-label="Profil"
-          >
-            <User size={24} />
-            <span>Profil</span>
-          </Link>
-        </li>
+
+        {user ? (
+          <li>
+            <Link
+              to="/user/$userId"
+              params={{ userId: user?.id?.toString() || "" }}
+              className="nav-link"
+              aria-label="Profil"
+            >
+              <User size={24} />
+              <span>Profil</span>
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link
+              to="/auth/sign-in"
+              search={{
+                redirect: location.pathname,
+              }}
+              className="nav-link"
+              aria-label="Connexion"
+            >
+              <User size={24} />
+              <span>Connexion</span>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
