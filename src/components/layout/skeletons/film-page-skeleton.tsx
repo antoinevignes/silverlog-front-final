245e2c0086d943739b375919d061b27af40a83f6
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-03-31 00:09:07 +0200

    correction

--- src/components/layout/skeletons/film-page-skeleton.tsx
+++ src/components/layout/skeletons/film-page-skeleton.tsx
@@ -4,9 +4,23 @@ import "./film-page-skeleton.scss";
 export default function FilmPageSkeleton() {
   return (
     <div className="film-page-skeleton">
-      {Array.from({ length: 6 }).map((_, i) => (
-        <Skeleton key={i} width={104} height={156} />
-      ))}
+      <div className="film-page-skeleton__mobile">
+        {Array.from({ length: 3 }).map((_, i) => (
+          <Skeleton key={i} width={104} height={156} />
+        ))}
+      </div>
+
+      <div className="film-page-skeleton__tablet">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <Skeleton key={i} width={104} height={156} />
+        ))}
+      </div>
+
+      <div className="film-page-skeleton__desktop">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <Skeleton key={i} width={104} height={156} />
+        ))}
+      </div>
     </div>
   );
 }
