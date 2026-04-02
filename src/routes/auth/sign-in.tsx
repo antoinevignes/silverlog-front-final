05a1518ba9f1e05bf7fd70cb205b3ff13e448d45
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-04-02 22:45:12 +0200

    correction schema Zod connexion

--- src/routes/auth/sign-in.tsx
+++ src/routes/auth/sign-in.tsx
@@ -34,7 +34,19 @@ function RouteComponent() {
     validators: {
       onChange: z.object({
         email: z.email("Email requis"),
-        password: z.string().trim().min(1, "Mot de passe requis"),
+        password: z
+          .string()
+          .trim()
+          .min(12, "Mot de passe trop court")
+          .max(128, "Mot de passe trop long")
+          .regex(/[A-Z]/, "Doit contenir au moins une majuscule")
+          .regex(/[a-z]/, "Doit contenir au moins une minuscule")
+          .regex(/\d/, "Doit contenir au moins un chiffre")
+          .regex(
+            new RegExp("[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?~`]"),
+            "Doit contenir un caractère spécial",
+          )
+          .refine((val) => !/\s/.test(val), "Ne doit pas contenir d'espace"),
       }),
     },
     onSubmit: async ({ value }) => {
